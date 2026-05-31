562b505cac4b824f1a6a67919f04254cbfd972a0
Lillykatragadda <katragaddalilly@gmail.com>
2026-05-31 20:51:12 +0530

    Mobile navbar fix

--- js/script.js
+++ js/script.js
@@ -112,3 +112,14 @@ if (menuToggle && navLinks) {
     navLinks.classList.toggle("active");
   });
 }
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 50) {
+    navbar.style.background = "#111";
+    navbar.style.boxShadow = "0 10px 30px rgba(0,0,0,0.2)";
+  } else {
+    navbar.style.background = "#111";
+    navbar.style.boxShadow = "none";
+  }
+});
